93f59e567ce3b216781e032811a3722f4559e01c
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2025-02-26 18:17:31 +0900

    update: 式と文の違いを関数呼び出しでの変数宣言から理解

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 fn main() {
-    let y = {
-        let x = 3; // なんらかの動作をして値を返さない命令である「文」
-        x + 1 // 結果値に評価される「式」、終端にセミコロンがない
+    // x には plus_one 関数の返り値として i32 型が入ることを期待
+    let x = plus_one(5);
 
-        // let x = (let y = 6);  // let = y = 6 は値を返さないので、エラーになる
-    };
+    println!("The value of x is: {}", x);
+}
 
-    // The value of y is: 4
-    println!("The value of y is: {}", y);
+fn plus_one(x: i32) -> i32 {
+    // 式なのでセミコロンはいらない、セミコロンがあると文になる
+    // 文にすると値を返さないので、let x の宣言で値が入らずエラーになる
+    x + 1
 }
