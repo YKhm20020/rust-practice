300b268473896c1b6484eba9e253797485c784d6
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2025-02-26 18:44:41 +0900

    update: let 文内での if 式における代入について理解

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,12 @@
 fn main() {
-    let number = 3;
+    let condition = true;
 
-    if number < 5 {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
-    }
+    // let 文内で if 式による条件分岐で代入する値を変えている
+    let number = if condition { 5 } else { 6 };
 
-    // 複数の条件を評価する場合
-    if number % 4 == 0 {
-        println!("number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("number is divisible by 3");
-    } else if number % 2 == 0 {
-        println!("number is divisible by 2");
-    } else {
-        println!("number is not divisible by 4, 3, or 2");
-    }
+    // この場合は if, else のそれぞれの型に互換性がない旨のエラー発生
+    // let number = if condition { 5 } else { "six" };
 
-    // number が 3 と評価され、bool 値でなくなっているためエラー発生
-    // if number {
-    //     println!("number was three");
-    // }
+    // The value of number is: 5
+    println!("The value of number is: {}", number);
 }
